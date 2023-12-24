352bbc34d597d89e7b45146ecbfd1155b1b9dbda
Erlangga Riansyah <erlangga.riansyah@ifg-life.id>
2023-12-24 22:00:21 +0700

    main push

--- components/HeaderSecondary.tsx
+++ components/HeaderSecondary.tsx
@@ -1,12 +1,20 @@
+import useLoginModal from "@/hooks/useLoginModal";
+import { useCallback } from "react";
 import { FaFeather, FaUser } from "react-icons/fa";
 
 const HeaderSecondary = () => {
+    const loginModal = useLoginModal();
+
+    const handleOnClickPost = useCallback(() => {
+        loginModal.onOpen();
+    }, [loginModal]);
+
     return (
         <div className="border-b-[1px] border-neutral-800 flex flex-row items-center">
             <div className="relative flex m-6">
                 <FaUser size={24} color="white" />
             </div>
-            <div className="relative flex px-4 py-2 rounded-full bg-neutral-800 hover:bg-opacity-90 w-full cursor-pointer transition">
+            <div onClick={handleOnClickPost} className="relative flex px-4 py-2 mr-6 rounded-full bg-neutral-800 hover:bg-opacity-90 w-full cursor-pointer transition">
                 <p className="text-center font-semibold text-white text-[20px]">Tulis sesuatu...</p>
             </div>
         </div>
