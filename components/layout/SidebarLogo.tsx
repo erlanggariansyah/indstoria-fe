777c9b39724ec41ebc4d6a27c6630bd61f8fd550
Erlangga Riansyah <erlangga.riansyah@ifg-life.id>
2023-12-24 22:45:43 +0700

    main push

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -9,7 +9,7 @@ const SidebarLogo = () => {
 
     return (
         <div className="rounded-full mt-4 p-4 flex items-center justify-center transition" onClick={handleOnClick}>
-            <img src="/indstoria-dark-tr.png" alt="Indstoria Logo" />
+            <img className="hidden md:block lg:block xl:block 2xl:block" src="/indstoria-dark-tr.png" alt="Indstoria Logo" />
             <p className="text-white text-md font-serif hidden 2xl:flex xl:flex md:flex">In history, we learn</p>
         </div>
     )
