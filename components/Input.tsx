c1a4bd30bfb86edaed7386ac900ea9f8a9439dcf
Erlangga Riansyah <erlangga.riansyah@ifg-life.id>
2023-12-24 22:41:02 +0700

    main push

--- components/Input.tsx
+++ components/Input.tsx
@@ -11,7 +11,7 @@ const Input: React.FC<InputProps> = ({ placeholder, value, type, disabled, onCha
         <>
             <label className="font-semibold text-white text-xl">{placeholder}</label>
             <input disabled={disabled} onChange={onChange} value={value} placeholder={placeholder} type={type}
-            className="w-full p-4 text-lg bg-black border-2 border-neutral-800 rounded-md outline-none text-white focus:border-slate-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"/>
+            className="w-full p-4 text-lg bg-black border-2 border-neutral-800 rounded-full outline-none text-white focus:border-slate-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"/>
         </>
     )
 }
