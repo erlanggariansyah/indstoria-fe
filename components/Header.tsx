850254d68a0b8d84798b36e8876b63a76bb1cb86
Erlangga Riansyah <erlangga.riansyah@ifg-life.id>
2023-12-30 17:45:46 +0700

    main push

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useCallback } from "react";
-import { BiArrowBack } from "react-icons/bi";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 interface HeaderProps {
     label?: string;
@@ -17,12 +17,23 @@ const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
     return (
         <div className="border-b-[1px] border-neutral-800 p-5">
             <div className="flex flex-row items-center gap-2">
-                {
-                    showBackArrow && (
-                        <BiArrowBack onClick={handleBack} color="white" size={20} className="cursor-pointer hover:opacity-70 transition" />
-                    )
-                }
-                <h1 className="text-white text-xl font-semibold">{label}</h1>
+                <div>
+                    {
+                        showBackArrow && (
+                            <SlArrowLeft onClick={handleBack} color="white" size={20} className="cursor-pointer hover:opacity-70 transition" />
+                        )
+                    }
+                </div>
+                <div className="justify-center w-full">
+                    <h1 className="text-white text-xl font-semibold text-center">{label}</h1>
+                </div>
+                <div>
+                    {
+                        showBackArrow && (
+                            <SlArrowRight onClick={handleBack} color="white" size={20} className="cursor-pointer hover:opacity-70 transition" />
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
--- components/HeaderSecondary.tsx
+++ components/HeaderSecondary.tsx
@@ -1,6 +1,7 @@
 import useLoginModal from "@/hooks/useLoginModal";
 import { useCallback } from "react";
-import { FaUser } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { FaPenToSquare } from "react-icons/fa6";
 
 const HeaderSecondary = () => {
     const loginModal = useLoginModal();
@@ -12,10 +13,13 @@ const HeaderSecondary = () => {
     return (
         <div className="border-b-[1px] border-neutral-800 flex flex-row items-center">
             <div className="relative flex m-6">
-                <FaUser size={24} color="white" />
+                <FaSearch size={24} color="white" />
             </div>
-            <div onClick={handleOnClickPost} className="relative flex px-4 py-2 mr-6 rounded-full bg-neutral-800 hover:bg-opacity-90 w-full cursor-pointer transition">
-                <p className="text-center font-light antialiased text-white text-[20px]">Tulis sesuatu...</p>
+            <div onClick={handleOnClickPost} className="relative flex px-4 py-2 mr-2 rounded-full bg-neutral-800 hover:bg-opacity-90 w-full cursor-pointer transition">
+                <p className="text-center font-light antialiased text-white text-[20px]">Cari sesuatu...</p>
+            </div>
+            <div className="relative flex m-6">
+                <FaPenToSquare size={24} color="white" />
             </div>
         </div>
     )
